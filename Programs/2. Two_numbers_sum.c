c8ed63afd9891e1883d999e6c9705adae3e4b3e4
TechCourses4u <113705835+TechCourses4u@users.noreply.github.com>
2022-09-22 21:49:06 +0530

    Input Output using scanf and printf
    added scanf to take input from user instead of hard coded values

--- Programs/2. Two_numbers_sum.c
+++ Programs/2. Two_numbers_sum.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 int main(){
-	int num1=10;
-	int num2=20;
-	int sum = num1 + num2;
-	printf("sum is %d",sum);
-	return 0;
+    int a;
+    int b;
+    int sum;
+    printf("enter 2 numbers:\n");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("sum of a and b is %d\n",sum);
+    return 0;
 }
+
+/* Output :
+enter 2 numbers:
+40 30
+sum of a and b is 70
+*/
