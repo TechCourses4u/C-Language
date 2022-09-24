0e970420eec2b1cff36b753843f532079cd05798
TechCourses4u <113705835+TechCourses4u@users.noreply.github.com>
2022-09-24 21:33:01 +0530

    Update 1. Month_mapping.c
    Added problem description

--- Practice Programs/1. Month_mapping.c
+++ Practice Programs/1. Month_mapping.c
@@ -1,3 +1,6 @@
+/* Write a program to map month number to their name.
+ * For eg - 1 - January 2- February and so on
+*/
 #include<stdio.h>
 int main(){
     int month;
@@ -5,41 +8,29 @@ int main(){
     scanf("%d",&month);
     if(month == 1){
         printf("Januaray\n");
-    }
-    else if(month == 2){
+    } else if(month == 2){
         printf("February\n");
-    }
-    else if(month == 3){
+    } else if(month == 3){
         printf("March\n");
-    }
-    else if(month == 4){
+    } else if(month == 4){
         printf("April\n");
-    }
-    else if(month == 5){
+    } else if(month == 5){
         printf("May\n");
-    }
-    else if(month == 6){
+    } else if(month == 6){
         printf("June\n");
-    }
-    else if(month == 7){
+    } else if(month == 7){
         printf("July\n");
-    }
-    else if(month == 8){
+    } else if(month == 8){
         printf("August\n");
-    }
-    else if(month == 9){
+    } else if(month == 9){
         printf("September\n");
-    }
-    else if(month == 10){
+    } else if(month == 10){
         printf("October\n");
-    }
-    else if(month == 11){
+    } else if(month == 11){
         printf("November\n");
-    }
-    else if(month == 12){
+    } else if(month == 12){
         printf("December\n");
-    }
-    else{
+    } else{
         printf("Invalid month\n");
     }
     return 0;
