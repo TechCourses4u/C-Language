22f839c2af3a380fa40ca8edb1e19a4cf63f9051
TechCourses4u <113705835+TechCourses4u@users.noreply.github.com>
2022-09-30 20:34:25 +0530

    Create 11. Logical1.c

---
+++ Programs/11. Logical1.c
@@ -0,0 +1,13 @@
+/*Sample program to understand logical operators better*/
+#include <stdio.h>
+int main()
+{
+    int a = 10, b = 20, c = 0;
+    printf("1st: %d \n", (a==10) && (b==20));
+    printf("2nd: %d \n", (a==10) && (b==10));
+    printf("3rd: %d \n", (a==10) || (b==10));
+    printf("4th: %d \n", !(a==10));
+    printf("5th: %d \n", (a==10) && c);
+    printf("6th: %d \n", !(a == b));
+    return 0;
+}
