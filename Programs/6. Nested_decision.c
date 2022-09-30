4d85bcbef6c5928186e7d20ab449f68325970fc0
TechCourses4u <113705835+TechCourses4u@users.noreply.github.com>
2022-09-30 20:30:43 +0530

    Create 6. Nested_decision.c

---
+++ Programs/6. Nested_decision.c
@@ -0,0 +1,30 @@
+/* sample file to practice nested if else conditions*/
+#include<stdio.h>
+int main(){
+    int raining;
+    int want_to_bath;
+    int want_to_rest;
+    printf(" Enter 1 if raining,otherwise enter 0\n");
+    scanf("%d",&raining);
+    if(raining){
+        printf(" Enter 1 want to bath, otherwise enter 0\n");
+        scanf("%d",&want_to_bath);
+        if(want_to_bath){
+            printf("go out and bath in rain");
+        }
+        else{
+            printf("stay home");
+        }
+    }
+    else{
+        printf(" Enter 1 want to rest, otherwise enter 0\n");
+        scanf("%d",&want_to_rest);
+        if(want_to_rest){
+            printf("stay home");
+        }
+        else{
+            printf("go shopping");
+        }
+    }
+    return 0;
+}
