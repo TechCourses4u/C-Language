11887ef5bb656e737ba3b15ff9d00d83b9200b8c
TechCourses4u <113705835+TechCourses4u@users.noreply.github.com>
2022-10-08 22:21:26 +0530

    Create 20. Nested_loops.c

---
+++ Programs/20. Nested_loops.c
@@ -0,0 +1,18 @@
+/* Sample program to print below pattern
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+*/
+#include<stdio.h>
+int main(){
+    int sum=0;
+    for(int row = 1; row<=5; row++){
+	    for(int col=1;col<=row;col++){
+            printf("%d ",col);
+        }
+        printf("\n");
+    }
+    return 0; 
+}
