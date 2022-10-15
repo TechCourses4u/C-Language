95235603f6b8b16c64a96618cce88db50622b36e
Tech Courses <113705835+TechCourses4u@users.noreply.github.com>
2022-10-16 00:25:03 +0530

    Update 28. switch.c

--- Programs/28. switch.c
+++ Programs/28. switch.c
@@ -7,6 +7,17 @@ Imp points:
 5. default block is optional
 6. continue is not available in switch
 7. case continue to execute until break is encountered.
+
+switch vs if else:
+- switch case can not use statements like case x less than 10
+- Similarly, expressions like 2+3 is allowed but a+b is not allowed
+- float is also not allowed in switch case
+- Switch is either more or  as efficient as if else
+- Compiler generates a jump table for switch during compilation
+- If else are slower because conditions are evaluated at run time.
+- For multiway branching i.e. more cases with constant values, switch is better
+- For variable boolean results, if else is better.
+- In real projects, switch is always preferred over if else when possible.
 */
 
 #include<stdio.h>
