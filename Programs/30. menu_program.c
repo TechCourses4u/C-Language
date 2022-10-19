52cc364669813f932a88e68da95ba96fc6cd0eba
Tech Courses <113705835+TechCourses4u@users.noreply.github.com>
2022-10-20 00:50:59 +0530

    Update and rename 30. menu.c to 30. menu_program.c

--- Programs/30. menu_program.c
+++ Programs/30. menu_program.c
@@ -10,7 +10,7 @@ Based on user choice, particular task is performed.
 #include<stdio.h>
 void even_odd(){
     int num;
-    printf("Enter number to check even/odd\n");
+    printf("Enter number:");
     scanf("%d",&num);
     if(num%2)
         printf("odd number\n");
@@ -20,43 +20,43 @@ void even_odd(){
 
 void prime(){
     int num;
-    printf("Enter number to check prime or not\n");
+    printf("Enter number:");
     scanf("%d",&num);
     for(int i=2;i<num;i++){
         if(num%i == 0){
-            printf("not a prime number\n");
+            printf("%d is not a prime number\n",num);
             return;
-        }        
+        }
     }
-    printf("Yes,it is a prime number\n");
+    printf("%d is a prime number\n",num);
 }
 
 void absolute(){
     int num;
-    printf("Enter number to check prime or not\n");
+    printf("Enter number:");
     scanf("%d",&num);
     if(num<0)
         num=-num;
-    printf("Absolute value is %d\n",num);
+    printf("absolute value is %d\n",num);
 }
 
 void square(){
     int num;
-    printf("Enter number to find square\n");
+    printf("Enter number:");
     scanf("%d",&num);
     printf("square of %d is %d\n",num,num*num);
-} 
+}
 
 int main(){
-    int choice=1;
-    char play_again='y';
+    int choice;
+    char choose_again;
     do{
         printf("Please choose from below options:\n");
         printf("1. Even/Odd check\n");
         printf("2. Prime number check\n");
         printf("3. Get Absolute value\n");
         printf("4. Find Square of a number\n");
-        printf("Enter your choice\n");
+        printf("Enter your choice : ");
         scanf("%d",&choice);
         switch(choice){
             case 1:
@@ -72,11 +72,11 @@ int main(){
                 square();
                 break;
             default:
-                printf("Invalid option chosen\n");
+                printf("Invalid choice\n");
         }
         fflush(stdin);
         printf("Do you want to choose again\n");
-        scanf("%c",&play_again);
-    } while(play_again=='y');            
-	return 0;
+        scanf("%c",&choose_again);
+    } while(choose_again=='y');
+    return 0;
 }
