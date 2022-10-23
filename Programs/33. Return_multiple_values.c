1b606b75eb7d079e4447862ac790382fc2dc443b
Tech Courses <113705835+TechCourses4u@users.noreply.github.com>
2022-10-23 23:54:02 +0530

    Create 33. Return_multiple_values.c

---
+++ Programs/33. Return_multiple_values.c
@@ -0,0 +1,15 @@
+/* Sample program to understand how to return multiple values from a function using call by reference */
+#include<stdio.h>
+int square_and_cube(int x,int* square,int* cube){
+    *square = x*x;
+    *cube = x*x*x;
+}
+
+int main(){
+    int a = 10;
+    int square,cube;
+    square_and_cube(a,&square,&cube);
+    printf("square of %d is %d\n",a,square);
+    printf("cube of %d is %d\n",a,cube);
+    return 0;
+}
