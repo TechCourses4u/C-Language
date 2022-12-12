9ed06d3a83173e915bea2b197932a0f1260c2bac
Tech Courses <113705835+TechCourses4u@users.noreply.github.com>
2022-12-12 23:30:46 +0530

    Update 76. Union.c

--- Programs/76. Union.c
+++ Programs/76. Union.c
@@ -1,4 +1,5 @@
 /* Sample program to understand union in C */
+/* Program 1 : Understanding Union */
 #include<stdio.h>
 union xy{
     int x;
@@ -24,3 +25,29 @@ int main(){
 
     return 0;
 }
+/* Output :
+sizeof(xy): 4
+after temp.x=4 : 
+x: 4, y: 4
+after temp.y=10 :
+x: 10, y: 10
+accessing via union ptr and union var: 
+x: 20, y: 20
+x: 20, y: 20
+*/
+
+/* Program 2 : size of Union is size of biggest member */
+#include<stdio.h>
+union xyz{
+    char x;
+    int y[10];
+    double z;
+};
+
+int main(){
+    printf("sizeof(xyz): %d\n",sizeof(union xyz));
+    return 0;
+}
+/*Output: 
+40
+*/
