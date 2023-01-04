0e66c896f7664954f066fba3cd605bbd067e7af7
Tech Courses <113705835+TechCourses4u@users.noreply.github.com>
2023-01-04 22:23:43 +0530

    Update 80. Typedef.c

--- Programs/80. Typedef.c
+++ Programs/80. Typedef.c
@@ -6,12 +6,18 @@ typedef struct student{
     char name[20];
     int roll_no;
     int age;
-}STUDENT;
+}STUDENT,*PSTUDENT;
 
 int main(){
     STUDENT st1 = {"rahul",300,25};
     printf("Name : %s\n",st1.name);
     printf("Roll No : %d\n",st1.roll_no);
     printf("Age : %d\n",st1.age);
+    printf("-----------------\n");
+
+    PSTUDENT ptr = &st1;
+    printf("Name : %s\n",ptr->name);
+    printf("Roll No : %d\n",ptr->roll_no);
+    printf("Age : %d\n",ptr->age);
     return 0;
 }
