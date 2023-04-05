c72d970857d776f94adc066ffbef3a388be603fb
Tech Courses <113705835+TechCourses4u@users.noreply.github.com>
2023-04-05 19:11:32 +0530

    Create 108. voidPointer.c

---
+++ Programs/108. voidPointer.c
@@ -0,0 +1,26 @@
+/* Sample program to understand void pointer in c */
+#include<stdio.h>
+int main(){
+    int a = 10;
+    void *vp = &a;
+
+    printf("sizeof(void) : %d\n",sizeof(void));
+    printf("sizeof(void*) : %d\n",sizeof(void*));
+    printf("sizeof(vp) : %d\n",sizeof(vp));
+
+    printf("initial address and value : %u %d\n",vp,*(int*)vp);
+    vp = vp+1;        // should not be used
+    printf("address with no typecast addition : %u\n",vp);
+    vp = (int *)vp + 1;     // ok
+    printf("address with typecast addition : %u\n",vp);
+    return 0;
+}
+
+/* Output :
+sizeof(void) : 1
+sizeof(void*) : 8
+sizeof(vp) : 8
+initial address and value : 6618644 10  
+address with no typecast addition : 6618645
+address with typecast addition : 6618649
+*/
