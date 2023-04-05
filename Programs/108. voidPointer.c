59e4578247877d41369c6ecaf77c98f9f81e4056
Tech Courses <113705835+TechCourses4u@users.noreply.github.com>
2023-04-05 19:24:45 +0530

    Update 108. voidPointer.c

--- Programs/108. voidPointer.c
+++ Programs/108. voidPointer.c
@@ -4,12 +4,12 @@ int main(){
     int a = 10;
     void *vp = &a;
 
-    printf("sizeof(void) : %d\n",sizeof(void));
+    printf("sizeof(void) : %d\n",sizeof(void));         // in gcc, sizeof(void) is 1
     printf("sizeof(void*) : %d\n",sizeof(void*));
     printf("sizeof(vp) : %d\n",sizeof(vp));
 
     printf("initial address and value : %u %d\n",vp,*(int*)vp);
-    vp = vp+1;        // should not be used
+    vp = vp+1;        // should not be used, not all compilers allow this
     printf("address with no typecast addition : %u\n",vp);
     vp = (int *)vp + 1;     // ok
     printf("address with typecast addition : %u\n",vp);
